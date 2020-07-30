88e142b49ba2d98ccf374325b9fe7a52133b75fc
liz-barnes <embarnes95@gmail.com>
2020-07-30 10:16:27 -0500

    add event listeners to randomize colors and change background color

--- app.js
+++ app.js
@@ -37,4 +37,82 @@ const colors = [
     colorName: 'Alice Blue',
     hexCode: '#f0f8ff',
   },
-]
+];
+
+const colorRandomizer = () => {
+  return colors[Math.floor(Math.random() * colors.length)];
+};
+
+const colorSelector = () => {
+  const randomColor = colorRandomizer();
+  document.body.style.backgroundColor = randomColor.hex;
+
+  document.getElementById(
+    "color-indicator"
+  ).innerHTML = `${randomColor.name} ${randomColor.hex}`;
+};
+
+const buttonClick = () => {
+  document
+    .getElementById("simpleButton")
+    .addEventListener("click", colorSelector);
+};
+
+
+
+const init = () => {
+  buttonClick();
+  
+};
+
+init();
+
+
+
+
+
+const handleButtonClick = () => {
+
+  const colorFlipper = (array) => {
+  return array[Math.floor(Math.random() * array.length)].hexCode;
+}
+
+  document.querySelector('body').style.backgroundColor = `#${colorFlipper(colorArraySimple)}`;
+}
+
+
+const buttonEvents = () => {
+  document.querySelector('#click-btn').addEventListener('click', handleButtonClick)
+}
+
+buttonEvents();
+
+
+
+const printToDom = (divID, textToPrint) => {
+  const selectedDiv = document.getElementById(divID);
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const addColorNameToPage = (color) => {
+
+  const textToPrint = `<div>Chosen Color: ${color}</div>`;
+  console.log(textToPrint);
+  printToDom('colorName', textToPrint);
+}
+
+const handleButtonClick = e => {
+  const buttonId = e.target.id;
+  const randomNum = Math.floor(Math.random() * Math.floor(10));
+  const chosenColor = colors[randomNum];
+  addColorNameToPage(chosenColor);
+  const bodySelector = (document.querySelector(
+      'body'
+  ).style.backgroundColor = chosenColor);
+};
+
+const buttonEvents = () => {
+  document.querySelector('#myBtn').addEventListener('click', handleButtonClick);
+};
+
+buttonEvents();
